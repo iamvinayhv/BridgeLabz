3adb17fad825fff5b744692f55476a2c67c32568
pranalipatil209 <pranalipatil209@gmail.com>
2016-04-25 08:58:26 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,76 @@
+angular.module('bridgeApp', ['ui.router'])
+    .config(function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/Home');
+
+        $stateProvider
+        .state('Home', {
+            url: '/Home',
+            views: {
+                nav: {
+                    templateUrl: 'pages/Navbar.html'
+                },
+                content: {
+                    templateUrl: 'pages/Home.html'
+                }
+            }
+        })
+        .state('AboutUs', {
+            url: '/AboutUs',
+            views: {
+                nav: {
+                    templateUrl: 'pages/Navbar.html'
+                },
+                content: {
+                    templateUrl: 'pages/AboutUs.html'
+                }
+            }
+        })
+        .state('OurJob', {
+            url: '/OurJob',
+            views: {
+                nav: {
+                    templateUrl: 'pages/Navbar.html'
+                },
+                content: {
+                    templateUrl: 'pages/OurJob.html'
+                }
+            }
+        })
+        .state('JoinUs', {
+            url: '/JoinUs',
+            views: {
+                nav: {
+                    templateUrl: 'pages/Navbar.html'
+                },
+                content: {
+                    templateUrl: 'pages/JoinUs.html'
+                }
+            }
+        })
+        .state('Contact', {
+            url: '/Contact',
+            views: {
+                nav: {
+                    templateUrl: 'pages/Navbar.html'
+                },
+                content: {
+                    templateUrl: 'pages/Contact.html'
+                }
+            }
+        })
+        .state('Login', {
+            url: '/Login',
+            views: {
+                nav: {
+                    templateUrl: 'pages/Navbar.html'
+                },
+                content: {
+                    templateUrl: 'pages/Login.html'
+                }
+            }
+        })
+
+    });
+
+
